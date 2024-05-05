63308dd9afc8cb1cefa1f10a1d5e83eea7c57970
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-05-05 17:20:56 +0600

    fix types3

--- src/components/Rewies.tsx
+++ src/components/Rewies.tsx
@@ -3,33 +3,44 @@ import React, { useState, useEffect } from 'react';
 import { FiveStarRating } from "./five-star-rating";
 import { fetchFeedbacks } from '@/app/api/api';
 
+interface FeedbackAttributes {
+  stars: number;
+  clientName: string;
+  feedbackText: string;
+}
+
+interface Item {
+  id: string;
+  attributes: FeedbackAttributes;
+}
+
 function Rewies() {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState<Item[]>([]);
 
     useEffect(() => {
         fetchFeedbacks().then((response) => setItems(response.data));
       }, []);
 
     return (
-        <section class="client_section layout_padding-bottom" style={{padding: "20px 0"}}>
-  <div class="container">
-    <div class="heading_container heading_center ">
+        <section className="client_section layout_padding-bottom" style={{padding: "20px 0"}}>
+  <div className="container">
+    <div className="heading_container heading_center ">
       <h2>
         Отзывы
       </h2> 
     </div>
-    <div id="carouselExample2Controls" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
+    <div id="carouselExample2Controls" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
       {items.map((item, i) => (
   <div key={i} className={`carousel-item ${i === 0 ? 'active' : ''}`}>
-    <div class="row">
-      <div class="col-md-11 col-lg-10 mx-auto">
-        <div class="box">
-          <div class="img-box">
+    <div className="row">
+      <div className="col-md-11 col-lg-10 mx-auto">
+        <div className="box">
+          <div className="img-box">
             <FiveStarRating rating={item.attributes.stars} />
           </div>
-          <div class="detail-box">
-            <div class="name">
+          <div className="detail-box">
+            <div className="name">
               <h6>
                 {item.attributes.clientName}
               </h6>
@@ -37,7 +48,7 @@ function Rewies() {
             <p>
               {item.attributes.feedbackText}
             </p>
-            <i class="fa fa-quote-left" aria-hidden="true"></i>
+            <i className="fa fa-quote-left" aria-hidden="true"></i>
           </div>
         </div>
       </div>
@@ -47,13 +58,13 @@ function Rewies() {
 
       </div>
       <div className="mx-auto" style={{display: 'flex', justifyContent: 'center'}}>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample2Controls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample2Controls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample2Controls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample2Controls" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
       </div>
     </div>
